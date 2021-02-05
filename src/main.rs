ecf016037ee2e240e06a2bac5f8bae52937c99fa
ouwenkg <2630582710@qq.com>
2021-02-05 11:44:18 +0800

    Add index offset calculator

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,65 @@
 //     println!("{}", e)
 // }
 
-macro_rules! recurrence {
-    ( a[n]: $sty: ty = $($inits:expr),+ , ... , $recur:expr ) => {
+// macro_rules! recurrence {
+//     ( a[n]: $sty: ty = $($inits:expr),+ , ... , $recur:expr ) => {
+//         struct Recurrence {
+//             mem: [u64; 2],
+//             pos: usize,
+//         }
+
+//         impl Itertor for Recurrence {
+//             type Item = u64;
+
+//             fn next(&mut self) -> Option<u64> {
+//                 if self.pos < 2 {
+//                     let next_val = self.mem[self.pos];
+//                     self.pos += 1;
+//                     Some(next_val)
+//                 } else {
+//                     let n = self.pos;
+//                     let next_val = a[n-1] + a[n-2];
+//                     // todo: append to mem
+//                     self.pos += 1;
+//                     Some(next_val)
+//                 }
+//             }
+//         }
+//         todo!();
+//     };
+// }
+
+fn main() {
+    let fib = {
+        use std::ops::Index;
+
         struct Recurrence {
             mem: [u64; 2],
             pos: usize,
         }
 
-        impl Itertor for Recurrence {
-            type Item = u64;
+        #[derive(Debug)]
+        struct IndexOffset<'a> {
+            slice: &'a [u64; 2],
+            offset: usize,
+        }
+        impl<'a> Index<usize> for IndexOffset<'a> {
+            type Output = u64;
 
+            #[inline(always)]
+            fn index<'b>(&'b self, index: usize) -> &'b u64 {
+                use std::num::Wrapping;
+                let index = Wrapping(index);
+                let offset = Wrapping(self.offset);
+                let window = Wrapping(2);
+                let real_index = index - offset + window;
+                &self.slice[real_index.0]
+            }
+        }
+
+        impl Iterator for Recurrence {
+            type Item = u64;
+            #[inline]
             fn next(&mut self) -> Option<u64> {
                 if self.pos < 2 {
                     let next_val = self.mem[self.pos];
@@ -21,17 +70,33 @@ macro_rules! recurrence {
                     Some(next_val)
                 } else {
                     let n = self.pos;
-                    let next_val = a[n-1] + a[n-2];
-                    // todo: append to mem
+                    let a = IndexOffset {
+                        slice: &self.mem,
+                        offset: n,
+                    };
+                    let next_val = a[n - 1] + a[n - 2];
+                    
+                    {
+                        use std::mem::swap;
+                        let mut swap_tmp = next_val;
+                        for i in (0..2).rev() { // 1, 0
+                            swap(&mut swap_tmp, &mut self.mem[i]);
+                        }
+                    }
+                    
                     self.pos += 1;
                     Some(next_val)
                 }
             }
-        }
-        todo!();
-    };
-}
+        };
 
-fn main() {
-    println!("Hello, world!");
+        Recurrence {
+            mem: [0, 1],
+            pos: 0,
+        }
+    };
+
+    for e in fib.take(9) {
+        println!("{}", e);
+    }
 }
